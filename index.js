c584cddac25264a2c39da6f67b9cab7557be0514
hayksaakian <fakest.id.ever@gmail.com>
2013-02-07 13:43:18 -0800

    fleshing out + in place edit

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
     // update_progress();
+    $('.edit').editable(function(value, settings) { 
+			console.log(this);
+			console.log(value);
+			console.log(settings);
+			return(value);
+		}, { 
+		});
+    upd();
 });
 function upd(){
+	// $('.progress .bar').progressbar();
   $('.progress .bar').progressbar({
-      display_text: 2,
+      display_text: 1,
       use_percentage: false,
-      update: function( current_percentage ) { console.log(current_percentage) },
-      done: function( ) { console.log('done progressing') },
-      fail: function( error_message ) { console.log(error_message) },
+      update: function( current_percentage ) {
+      	console.log(current_percentage)
+      	// $(this).text($(this).text()+'a')
+      },
+      done: function(){
+      	console.log('done progressing')
+      	// $(this).text($(this).text()+'a')
+    	},
+      fail: function(error_message){
+      	console.log(error_message)
+      }
   });
 }
