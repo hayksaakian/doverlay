327ee4ca79cb50fedc4c81b40af78be6aac4f4dd
hayksaakian <fakest.id.ever@gmail.com>
2013-02-10 14:32:57 -0800

    working stuff, ui changes

--- index.js
+++ index.js
@@ -1,29 +1,179 @@
 $(document).ready(function() {
-    // update_progress();
-    $('.edit').editable(function(value, settings) { 
-			console.log(this);
-			console.log(value);
-			console.log(settings);
-			return(value);
-		}, { 
-		});
-    upd();
+  // update_progress();
+  var w = new Stopwatch(updateClock, 0.1); 
+  $('#start').click(function(){
+    w.start();
+    $(this).hide()
+    $('#pause').show()
+    $('#timer_container').removeClass('btn-danger');
+    $('#timer_container').removeClass('btn-info');
+    $('#timer_container').addClass('btn-inverse');
+  });
+  $('#pause').click(function(){
+    w.stop();
+    $(this).hide()
+    $('#start').show()
+    $('#timer_container').removeClass('btn-inverse');
+    $('#timer_container').addClass('btn-info');
+  });
+  $('#pause').hide();
+
+  $('#reset').click(function(){
+    $('#pause').click();
+    w.reset();
+    $('#timer_container').removeClass('btn-info');
+    $('#timer_container').addClass('btn-danger');
+  });
+
+
+
+  $('.edit').editable(function(value, settings) { 
+		console.log(this);
+		console.log(value);
+		console.log(settings);
+		return(value);
+	}, { 
+	});
+  upd();
+
+  // change pb color
+  // $('#pb').css({
+	//   'background-image': 'none',
+	//   'background-color': 'red'
+	// });
+  var sample_runner = {}
+  sample_runner.id = Math.random().toString(36).slice(2)
+  sample_runner.name = 'Testrunner'
+  sample_runner.stream_url = 'http://twitch.tv/testrunner'
+  sample_runner.color = 'red'
+  sample_runner.current_split = 1
+
+  var sample_run = {}
+  sample_run.splits = 's o m e t h i n g'.split(' ')
+  // splits will be an array of split objects
+  // TODO: define what a split really is
+  add_runner(sample_runner, sample_run);
+  $(document).on('click', '#runner_rows_controls #add', function(e){
+    the_input = $('#runner_rows_controls')
+    a_runner = {}
+    a_runner.id = Math.random().toString(36).slice(2)
+    a_runner.name = the_input.find('#name').val()
+    a_runner.color = the_input.find('#color').val()
+    a_runner.stream_url = the_input.find('#stream_url').val()
+    a_runner.current_split = the_input.find('#current_split').val()
+
+    add_runner(a_runner, sample_run)
+    // should check success
+    the_input.find('#name').val('')
+    the_input.find('#color').val('')
+    the_input.find('#stream_url').val('')
+    the_input.find('#current_split').val('0')
+
+  });
 });
-function upd(){
+function add_runner(runner, run){
+  console.log(runner)
+  // make a label
+  var label = $('#runner_name_label_template').clone();
+  label.attr(runner.id+'_label')
+  label.find('p').text(runner.name)
+  label.find('a').attr('href', runner.stream_url)
+  label.appendTo('#runner_name_labels')
+  label.show();
+
+  // make their progress bar
+  var bar = $('#runner_progress_bar_template').clone();
+  bar.attr('id', runner.id+'_progressbar')
+  the_bar = bar.find('.bar');
+  the_bar.attr('data-amount-total', run.splits.length)
+  the_bar.attr('data-amount-part', runner.current_split)
+  // pick a nice color, we may need it later  
+  var nice_colors = ['blue', 'purple', 'gray']
+  var nice_color = nice_colors[Math.floor(Math.random() * nice_colors.length)];
+  the_bar.css({
+    'background-image': 'none',
+    'background-color': ((runner.color) ? runner.color : false) || nice_color
+  });
+  if(the_bar.css('background-color') != 'rgb(255, 255, 255)' || runner.color == 'white'){
+  }else{
+    the_bar.css('background-color', nice_color);
+  }
+  bar.show();
+  bar.appendTo('#runner_progress_bars')
+  upd();
+
+  // add them to the table
+  var row = $('#runner_row_template').clone();
+  row.attr('id', runner.id+'_row')
+  row.find('#name').text(runner.name)
+  row.find('#color').text(runner.color)
+  row.find('#stream_url').text(runner.stream_url)
+  row.find('#current_split').text(runner.current_split)
+  row.find('#total_splits').text(run.splits.length)
+  row.find('#color').text(runner.color)
+  // make the buttons work
+  // increment
+  row.find('#increment_split').click(function(){
+    var br = $('#'+runner.id+'_progressbar .bar')
+    var n = parseInt(br.attr('data-amount-part'))
+    var ns = (n + 1 <= parseInt(br.attr('data-amount-total'))) ? n + 1 : n 
+    if(ns != n){
+      br.attr('data-amount-part', ns)
+      upd(br)
+      $('#'+runner.id+'_row #current_split').text(ns)
+    }
+  });
+  row.find('#decrement_split').click(function(){
+    var br = $('#'+runner.id+'_progressbar .bar')
+    var n = br.attr('data-amount-part')
+    var ns = (n - 1 >= 0) ? n - 1 : n 
+    if(ns != n){
+      br.attr('data-amount-part', ns)
+      upd(br)
+      $('#'+runner.id+'_row #current_split').text(ns)
+    }
+  });
+  row.find('#destroy').click(function(){
+    $('#'+runner.id+'_row').remove()
+    $('#'+runner.id+'_progressbar').remove()
+  });
+  // decrement
+  // remove
+
+  row.prependTo('#runner_rows')
+  row.show();
+
+}
+function upd(a_specific_bar){
 	// $('.progress .bar').progressbar();
-  $('.progress .bar').progressbar({
+  if(a_specific_bar){
+  }else{
+    a_specific_bar = $('.progress .bar');
+  }
+  a_specific_bar.progressbar({
       display_text: 1,
       use_percentage: false,
       update: function( current_percentage ) {
-      	console.log(current_percentage)
+      	// console.log(current_percentage)
       	// $(this).text($(this).text()+'a')
       },
       done: function(){
-      	console.log('done progressing')
+      	// console.log('done progressing')
       	// $(this).text($(this).text()+'a')
     	},
       fail: function(error_message){
       	console.log(error_message)
       }
   });
+}
+
+function updateClock(w) {
+    var watch = w.getElapsed();
+    $('#timer').html(watch.hours+':'+paddy(watch.minutes, 2)+':'+paddy(watch.seconds, 2)+'.'+paddy(watch.milliseconds, 2));
+}
+
+function paddy(n, p, c) {
+  var pad_char = typeof c !== 'undefined' ? c : '0';
+  var pad = new Array(1 + p).join(pad_char);
+  return (pad + n).slice(-pad.length);
 }
