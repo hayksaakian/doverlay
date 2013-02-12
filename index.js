873b8dc53f5a35bb012e9667f8d76977a1f0b3dc
hayksaakian <fakest.id.ever@gmail.com>
2013-02-11 20:25:29 -0800

    importing a o k

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+  // import_srl_races_list();
+  $('#import_list').click(function(){
+    $(this).attr('disabled', true);
+    import_srl_races_list();
+  });
+  $('#do_import').click(function(){
+    var selection = $('#race_select').val();
+    console.log('importing '+selection+'...')
+    if(selection == 'custom_game'){
+      console.log('no import, playing custom_game')
+    }else{
+      import_race(selection);
+    }
+  });
+
   // update_progress();
   var w = new Stopwatch(updateClock, 0.1); 
   $('#start').click(function(){
@@ -33,6 +49,8 @@ $(document).ready(function() {
       $('.bar').attr('data-amount-total', value)
       $('#total_splits').text(value)
       upd();
+    }else if(this.id == 'color'){
+      $(this).parent().id
     }
 		return(value);
 	}, { 
@@ -50,7 +68,7 @@ $(document).ready(function() {
   sample_runner.id = Math.random().toString(36).slice(2)
   sample_runner.name = 'Testrunner'
   sample_runner.stream_url = 'http://twitch.tv/testrunner'
-  sample_runner.color = 'red'
+  sample_runner.color = random_color();
   sample_runner.current_split = 1
 
   var sample_run = {}
@@ -115,7 +133,7 @@ $(document).ready(function() {
 
 });
 function add_runner(runner, run){
-  console.log(runner)
+  // console.log(runner)
   // make a label
   var label = $('#runner_name_label_template').clone();
   label.attr('id', runner.id+'_label')
@@ -131,30 +149,23 @@ function add_runner(runner, run){
   the_bar.attr('data-amount-total', run.splits)
   the_bar.attr('data-amount-part', runner.current_split)
   // pick a nice color, we may need it later  
-  var nice_colors = ['blue', 'purple', 'gray']
-  var nice_color = nice_colors[Math.floor(Math.random() * nice_colors.length)];
   the_bar.css({
     'background-image': 'none',
-    'background-color': ((runner.color) ? runner.color : false) || nice_color
+    'background-color': ((runner.color) ? runner.color : false) || random_color()
   });
   if(the_bar.css('background-color') != 'rgb(255, 255, 255)' || runner.color == 'white'){
   }else{
-    the_bar.css('background-color', nice_color);
+    the_bar.css('background-color', random_color());
   }
   bar.show();
   bar.appendTo('#runner_progress_bars')
   upd();
 
   // add them to the table
-  var row = $('#runner_row_template').clone();
-  row.attr('id', runner.id+'_row')
   runner['total_splits'] = run.splits
-
-  var output = Mustache.render(row.html(), runner);
-
-  row.html(output)
-  $(row).prependTo('#runner_rows')
-  row.show();
+  var row = $('#runner_row_template').html();
+  var output = Mustache.render(row, runner);
+  $('#runner_rows').prepend(output)
 
 }
 function upd(a_specific_bar){
@@ -189,4 +200,52 @@ function paddy(n, p, c) {
   var pad_char = typeof c !== 'undefined' ? c : '0';
   var pad = new Array(1 + p).join(pad_char);
   return (pad + n).slice(-pad.length);
+}
+
+function import_srl_races_list(){
+  $.getJSON('http://speedrunslive.com:81/races/', function(result){
+    console.log(result)
+    var races = result.races
+    if(races.length != null && races.length > 0){
+      var race_select = $('#race_select');
+      race_select.find('option').not('#custom_game').remove();
+      var template = $('#race_option').html();
+      for (var i = races.length - 1; i >= 0; i--) {
+        race_select.append(Mustache.render(template, races[i]));
+      };
+    }
+    $('#import_list').attr('disabled', false);
+  });
+}
+
+function import_race(race_id){
+  console.log(race_id+': race_id')
+  $.getJSON('http://speedrunslive.com:81/races/'+race_id, function(result){
+    console.log(result)
+    var the_race = {}
+    the_race.splits = 10
+    var entrants = Object.keys(result['entrants'])
+    var runners = []
+    for (var i = entrants.length - 1; i >= 0; i--) {
+      x = result['entrants'][entrants[i]]
+      the_runner = {}
+      the_runner.id = (x['twitch'] == null || x['twitch'] == undefined) ? random_id() : x['twitch']
+      the_runner.name = x['displayname']
+      the_runner.stream_url = 'http://twitch.tv/'+x['twitch']
+      the_runner.color = random_color();
+      the_runner.current_split = 0
+      add_runner(the_runner, the_race);
+    };
+  });
+}
+
+function random_id(){
+  return Math.random().toString(36).slice(2);
+}
+
+function random_color(){
+  var s = 'aqua, black, blue, fuchsia, gray, grey, green, maroon, navy, olive, purple, red, silver'
+  var c = s.split(', ')
+  var nice_color = c[Math.floor(Math.random() * c.length)];
+  return nice_color
 }
